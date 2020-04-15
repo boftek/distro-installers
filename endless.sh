ef1f63d260d381986f224130d2074a7f8f963c07
Philip Panić <63736527+philippanic@users.noreply.github.com>
2020-04-15 18:11:50 +0200

    Software installer for Endless OS

---
+++ endless.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# INSTALL CORE APPS FOR philip
+
+flatpak install flathub com.spotify.Client
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub com.visualstudio.code
+flatpak install flathub com.github.IsmaelMartinez.teams_for_linux
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.mojang.Minecraft
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub org.videolan.VLC
