55ccdf270bfedb434a0d85fb6efcdd11d674317e
philippanic <panic.philip@gmail.com>
2020-04-16 17:15:23 +0200

    Added automatic auth when installing

--- dpkg.sh
+++ dpkg.sh
@@ -11,19 +11,19 @@ wget https://dl.strem.io/linux/v4.4.54/stremio_4.4.52-1_amd64.deb -O stremio_4.4
 wget https://go.microsoft.com/fwlink/p/?linkid=2112886 -O teams.deb
 wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb -O steam.deb
 sudo dpkg -i Minecraft.deb
-sudo apt-get install -f
+sudo apt-get -y install -f
 sudo dpkg -i skypeforlinux-64.deb
-sudo apt-get install -f 
+sudo apt-get -y install -f 
 sudo dpkg -i discord.deb
-sudo apt-get install -f
+sudo apt-get -y install -f
 sudo dpkg -i stremio_4.4.52-1_amd64.deb
-sudo apt-get install -f
+sudo apt-get -y install -f
 sudo dpkg -i teams.deb
-sudo apt-get install -f
+sudo apt-get -y install -f
 sudo dpkg -i code.deb
-sudo apt-get install -f
+sudo apt-get -y install -f
 sudo dpkg -i steam.deb
-sudo apt-get install -f
+sudo apt-get -y install -f
 
 cd
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
